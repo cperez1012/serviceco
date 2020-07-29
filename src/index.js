8e774fad9d82073415d914c3beab48abf6467d68
cperez1012 <perezchristian1012@gmail.com>
2020-07-29 18:06:11 -0400

    added notes.md with project requirements

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//STORE -> A GLOBALIZED STATE
+
+//ACTION - INCREMENT
+
+//REDUCER - 
+
+//DISPATCH
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
