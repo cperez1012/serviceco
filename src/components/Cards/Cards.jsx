e7ffc167caf2f345b60c9cefcf5f69e072bff396
cperez1012 <perezchristian1012@gmail.com>
2020-08-01 16:39:43 -0400

    added first card for infected within Cards.jsx file

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Card, Grid, CardContent, Typography } from '@material-ui/core';
+
+import styles from './Cards.module.css';
 
 const Cards = (props) => {
 
     console.log(props);
 
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5">Real Data</Typography>
+                        <Typography color="textSecondary">Real Date</Typography>
+                        <Typography variant="body2">Number of active COVID-19 cases</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
