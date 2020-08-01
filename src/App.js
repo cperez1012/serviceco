72bb355daa90f9efc980a697512e72184bdd767f
cperez1012 <perezchristian1012@gmail.com>
2020-07-31 23:45:03 -0400

    created API folder and index.js file within folder, updated App.module.css folder with first body and container styling, updating App.js file to pull needed data

--- src/App.js
+++ src/App.js
@@ -9,8 +9,22 @@ import React from 'react';
 
 import { Cards, Chart, CountryChoice } from './components';
 import styles from './App.module.css';
+import { fetchData } from './api'
 
 class App extends React.Component {
+
+  state = {
+    data: {},
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({
+      data: fetchedData
+    });
+  }
+
   render() {
     return (
       <div className={styles.container}>
