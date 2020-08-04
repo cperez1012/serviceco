0381a76a5bc4406d21749f2a7e1844974ac8107b
cperez1012 <perezchristian1012@gmail.com>
2020-08-04 02:25:46 -0400

    updated api index.js file to have daily data of global covid-19 cases, added line chart with daily data of covid-19 cases and styling of page

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -5,31 +5,45 @@ import { Line, Bar } from 'react-chartjs-2';
 import styles from './Chart.module.css';
 
 const Chart = () => {
-    const [dailyData, setDailyData] = useState({});
+    const [dailyData, setDailyData] = useState([]);
 
-    useEffect(async() => {
+    useEffect(() => {
         const fetchAPI = async () => {
             setDailyData(await fetchDailyData());
         }
 
         fetchAPI();
     // }, [dailyData]);
-    })
+    });
 
         const lineChart = (
-            dailyData[0]
+            dailyData.length
                 ?(
                     <Line 
                     data={{
-                        labels: dailyData(({ date }) => date),
-                        datasets: [{}, {}]
+                        labels: dailyData.map(({ date }) => date),
+                        datasets: [{
+                            data: dailyData.map(({confirmed}) => confirmed),
+                            label: 'Infected',
+                            borderColor: '#3333ff',
+                            fill: true,
+                        }, {
+                            data: dailyData.map(({ deaths }) => deaths),
+                            label: 'Deaths',
+                            borderColor: 'red',
+                            backgroundColor: 'rbga(255, 0, 0, 0.5)',
+                            fill: true,
+                        }]
                     }}
                     />) : null
         );
 
     return (
-        <h1>Chart</h1>
-    )
+        // <h1>Chart</h1>
+        <div className={styles.container}>
+            {lineChart}
+        </div>
+        )
 }
 
 export default Chart;
