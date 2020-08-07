d62482bed07aade46d25b225452566797193a93f
cperez1012 <perezchristian1012@gmail.com>
2020-08-07 12:55:26 -0400

    removed App.css file, updated item md size within Grids within Cards.module.css file, and updated CountryChoice.module.css with formControl

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -13,7 +13,7 @@ if(!confirmed) {
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
-                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
+                <Grid item component={Card} xs={12} md={12} className={cx(styles.card, styles.infected)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">
@@ -30,7 +30,7 @@ if(!confirmed) {
                 </Grid>
             </Grid>
             <Grid container spacing={3} justify="center">
-                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>                    <CardContent>
+                <Grid item component={Card} xs={12} md={12} className={cx(styles.card, styles.recovered)}>                    <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5">
                             <CountUp
@@ -46,7 +46,7 @@ if(!confirmed) {
                 </Grid>
             </Grid>
             <Grid container spacing={3} justify="center">
-                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>                    <CardContent>
+                <Grid item component={Card} xs={12} md={12} className={cx(styles.card, styles.deaths)}>                    <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5">
                             <CountUp
