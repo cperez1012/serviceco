82929f1e4af1695d973373c7f6798220c0c9deb4
cperez1012 <perezchristian1012@gmail.com>
2020-08-12 22:39:44 -0400

    installed react bootstrap and working on three routes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,106 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import logo from './logo.svg';
+import styles from './App.module.css';
 
 
 // import Cards from './components/Cards/Cards';
 // import Chart from './components/Chart/Chart';
 // import CountryChoice from './components/CountryChoice/CountryChoice';
 
-import { Cards, Chart, CountryChoice } from './components';
-import styles from './App.module.css';
-import { fetchData } from './api';
-import image from './images/COVID-19 icon.png';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
-class App extends React.Component {
-
-  state = {
-    data: {},
-    country: '',
-  }
-
-  async componentDidMount() {
-    const fetchedData = await fetchData();
-
-    this.setState({data: fetchedData});
-  }
-
-  handleCountryChange = async (country) => {
-
-    const fetchedData = await fetchData(country);
-    //fetch the data
-    //set the state
-    this.setState({data: fetchedData, country: country});
-  }
-
-  render() {
-
-    const { data, country } = this.state;
-
-    return (
-      <div className={styles.container}>
-        <img className={styles.image} src={image} alt="COVID-19"/>
-        <Cards data={data}/>
-        <CountryChoice handleCountryChange={ this.handleCountryChange }/>
-        <Chart data={data} country={country}/>
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+             {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-    )
-  }
+    </Router>
+  );
 }
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+// import { Cards, Chart, CountryChoice } from './components';
+// import styles from './App.module.css';
+// import { fetchData } from './api';
+// import image from './images/COVID-19 icon.png';
+
+// class App extends React.Component {
+
+//   state = {
+//     data: {},
+//     country: '',
+//   }
+
+//   async componentDidMount() {
+//     const fetchedData = await fetchData();
+
+//     this.setState({data: fetchedData});
+//   }
+
+//   handleCountryChange = async (country) => {
+
+//     const fetchedData = await fetchData(country);
+//     //fetch the data
+//     //set the state
+//     this.setState({data: fetchedData, country: country});
+//   }
+
+//   render() {
+
+//     const { data, country } = this.state;
+
+//     return (
+//       <div className={styles.container}>
+//         <img className={styles.image} src={image} alt="COVID-19"/>
+//         <Cards data={data}/>
+//         <CountryChoice handleCountryChange={ this.handleCountryChange }/>
+//         <Chart data={data} country={country}/>
+//       </div>
+//     )
+//   }
+// }
 
 // function App() {
 //   return (
@@ -69,4 +123,4 @@ class App extends React.Component {
 //   );
 // }
 
-export default App;
+// export default App;
